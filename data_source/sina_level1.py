2a189c38e9352fe474e44993c91fa328790f38fe
bullud <bullud@126.com>
2015-12-22 19:51:18 +0800

    add get_data

--- data_source/sina_level1.py
+++ data_source/sina_level1.py
@@ -5,7 +5,7 @@ import time
 import urllib
 
 class SinaLevel1(threading.Thread):
-    def __init__(self):
+    def __init__(self, monitor_list, interval, ):
         threading.Thread.__init__(self)
 
     def run(self):
@@ -27,20 +27,7 @@ class SinaLevel1(threading.Thread):
                 httpClient.close()
 
 def main():
-   print('Hello world!')
-   httpClient = None
-   try:
-      httpClient = http.client.HTTPConnection('hq.sinajs.cn')
-      httpClient.request("GET", "/list=sz002466")
-      res = httpClient.getresponse()
-      print(res.status)
-      print(res.msg)
-      print(res.read().decode('gbk'))
-   except Exception as e:
-      print(e)
-   finally:
-      if httpClient:
-         httpClient.close()
+
 
 if __name__ == '__main__':
     main()
