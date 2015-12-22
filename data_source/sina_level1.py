b921803e8429ece096049a44506ac1a4cc2d4747
bullud <bullud@126.com>
2015-12-22 20:29:48 +0800

    add get_data

--- data_source/sina_level1.py
+++ data_source/sina_level1.py
@@ -5,8 +5,15 @@ import time
 import urllib
 
 class SinaLevel1(threading.Thread):
-    def __init__(self, monitor_list, interval, ):
+    def __init__(self, monitor_list, interval):
         threading.Thread.__init__(self)
+        self.base_url = 'hq.sinajs.cn'
+        self.query_list = '/list='
+
+        for code in monitor_list:
+            self.query_list += code + ','
+
+        print(code)
 
     def run(self):
         self.get_data()
@@ -14,7 +21,7 @@ class SinaLevel1(threading.Thread):
     def get_data(self):
         httpClient = None
         try:
-            httpClient = http.client.HTTPConnection('hq.sinajs.cn')
+            httpClient = http.client.HTTPConnection(self.base_url)
             httpClient.request("GET", "/list=sz002466")
             res = httpClient.getresponse()
             print(res.status)
@@ -27,7 +34,8 @@ class SinaLevel1(threading.Thread):
                 httpClient.close()
 
 def main():
-
+    monitor_list = ['sz002466', 'sz002460', 'sz300073', 'sz000558', 'sz300151', 'sz000952']
+    sl1 = SinaLevel1(monitor_list, 2)
 
 if __name__ == '__main__':
     main()
