3ae69948914d3f6981e753b7a866b6e0ebf38da1
bullud <bullud@126.com>
2015-12-23 19:35:37 +0800

    add get_data

--- data_source/sina_level1.py
+++ data_source/sina_level1.py
@@ -8,25 +8,33 @@ class SinaLevel1(threading.Thread):
     def __init__(self, monitor_list, interval):
         threading.Thread.__init__(self)
         self.base_url = 'hq.sinajs.cn'
-        self.query_list = '/list='
+        self.query = '/list='
 
         for code in monitor_list:
-            self.query_list += code + ','
-
-        print(code)
+            self.query += code + ','
 
     def run(self):
         self.get_data()
+
+    def stop(self):
+        self.thread_stop = True
+
+    def parse_data(self, data):
+        print(data)
 
     def get_data(self):
         httpClient = None
         try:
             httpClient = http.client.HTTPConnection(self.base_url)
-            httpClient.request("GET", "/list=sz002466")
+            httpClient.request("GET", self.query)
             res = httpClient.getresponse()
-            print(res.status)
-            print(res.msg)
-            print(res.read().decode('gbk'))
+            #print(res.status)
+            if res.status == 200:
+                data = res.read().decode('gbk')
+                self.parse_data(data)
+            else:
+                print(res.status)
+                return
         except Exception as e:
             print(e)
         finally:
@@ -34,8 +42,11 @@ class SinaLevel1(threading.Thread):
                 httpClient.close()
 
 def main():
-    monitor_list = ['sz002466', 'sz002460', 'sz300073', 'sz000558', 'sz300151', 'sz000952']
-    sl1 = SinaLevel1(monitor_list, 2)
+    monitor_list = ['sz002466', 'sz002460', 'sz300073', 'sz000558', 'sz300151', 'sz000952', 'sz000004', 'sz002421']
+    sinaL1 = SinaLevel1(monitor_list, 2)
+    sinaL1.start()
+    time.sleep(3)
+    sinaL1.stop()
 
 if __name__ == '__main__':
     main()
