164d9b41f572ae441db10070a994f738f8a22f18
bullud <bullud@126.com>
2015-12-24 18:33:17 +0800

    add bid

---
+++ common/bid.py
@@ -0,0 +1,44 @@
+from peewee import *
+class bid(Model):
+    code = IntegerField()
+    name = CharField()
+    to_open_price = FloatField()
+    ye_close_price = FloatField()
+    cur_price = FloatField()
+    to_high_price = FloatField()
+    to_low_price  = FloatField()
+    traded_share  = IntegerField()
+    traded_money  = FloatField()
+    buy1_count    = IntegerField()
+    buy1_price    = FloatField()
+    buy2_count    = IntegerField()
+    buy2_price    = FloatField()
+    buy3_count    = IntegerField()
+    buy3_price    = FloatField()
+    buy4_count    = IntegerField()
+    buy4_price    = FloatField()
+    buy5_count    = IntegerField()
+    buy5_price    = FloatField()
+    buy6_count    = IntegerField()
+    buy6_price    = FloatField()
+    buy7_count    = IntegerField()
+    buy7_price    = FloatField()
+    buy8_count    = IntegerField()
+    buy8_price    = FloatField()
+    buy9_count    = IntegerField()
+    buy9_price    = FloatField()
+    buy10_count   = IntegerField()
+    buy10_price   = FloatField()
+    date_time     = DateTimeField()
+    #time          = TimeField()
+
+    def __init__(self):
+        return
+
+
+def main():
+
+    return
+
+if __name__ == '__main__':
+    main()
--- data_source/sina_level1.py
+++ data_source/sina_level1.py
@@ -2,7 +2,9 @@
 import http.client
 import threading
 import time
+import datetime
 import urllib
+import common.bid
 
 class SinaLevel1(threading.Thread):
     def __init__(self, monitor_list, interval):
@@ -20,7 +22,58 @@ class SinaLevel1(threading.Thread):
         self.thread_stop = True
 
     def parse_data(self, data):
-        print(data)
+        rows = data.split('\n')
+        for row in rows:
+            row = row[11:]
+            bd = common.bid
+            bd.code = row[:8]
+
+            subrow = row[10:len(row) - 2]
+            print(subrow)
+            items = subrow.split(',')
+            if len(items) == 33:
+                bd.name = items[0]
+
+                bd.to_open_price  = float(items[1])
+                bd.ye_close_price = float(items[2])
+                bd.cur_price      = float(items[3])
+                bd.to_high_price  = float(items[4])
+                bd.to_low_price   = float(items[5])
+                #bd.buy_1_price   = items[6]
+                #bd.sell_1_price  = items[7]
+                bd.traded_share   = int(items[8])
+                bd.traded_money   = float(items[9])
+
+                bd.buy1_count     = int(items[10])
+                bd.buy1_price     = float(items[11])
+                bd.sell1_count    = int(items[20])
+                bd.sell1_price    = float(items[21])
+
+                bd.buy2_count     = int(items[12])
+                bd.buy2_price     = float(items[13])
+                bd.sell2_count    = int(items[22])
+                bd.sell2_price    = float(items[23])
+
+                bd.buy3_count     = int(items[14])
+                bd.buy3_price     = float(items[15])
+                bd.sell3_count    = int(items[24])
+                bd.sell3_price    = float(items[25])
+
+                bd.buy4_count     = int(items[16])
+                bd.buy4_price     = float(items[17])
+                bd.sell4_count    = int(items[26])
+                bd.sell4_price    = float(items[27])
+
+                bd.buy5_count     = int(items[18])
+                bd.buy5_price     = float(items[19])
+                bd.sell5_count    = int(items[28])
+                bd.sell5_price    = float(items[29])
+
+                #print(items[30] + ' ' + items[31])
+                bd.date_time = datetime.datetime.strptime(items[30] + ' ' + items[31], '%Y-%m-%d %H:%M:%S')
+                print(bd.date_time)
+
+
 
     def get_data(self):
         httpClient = None
@@ -42,7 +95,8 @@ class SinaLevel1(threading.Thread):
                 httpClient.close()
 
 def main():
-    monitor_list = ['sz002466', 'sz002460', 'sz300073', 'sz000558', 'sz300151', 'sz000952', 'sz000004', 'sz002421']
+    monitor_list = ['sz002466']
+    #, 'sz002460', 'sz300073', 'sz000558', 'sz300151', 'sz000952', 'sz000004', 'sz002421']
     sinaL1 = SinaLevel1(monitor_list, 2)
     sinaL1.start()
     time.sleep(3)
