7b676f49a15961d52233f3cad6162f0c8c5f6d4a
bullud <bullud@126.com>
2016-01-17 23:58:51 +0800

    Create MplMainWindow

---
+++ common/oneMinute.py
@@ -0,0 +1,12 @@
+from peewee import *
+from datetime import date
+
+class oneMinute(Model):
+    code = CharField(default='000000')
+    open_price  = FloatField(default=0)
+    close_price = FloatField(default=0)
+    high_price = FloatField(default=0)
+    low_price  = FloatField(default=0)
+    traded_share = IntegerField(default=0)
+    traded_money = IntegerField(default=0)
+    minuteT      = DateTimeField(default=date(1980, 10, 24))
