18dff18e75ec0b2eb1d7c8d1e2053b334b0b7337
bullud <bullud@126.com>
2016-01-17 23:42:02 +0800

    Create MplMainWindow

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='AutoTrader',
+    version='',
+    packages=['test', 'common', 'data_source', 'data_process'],
+    url='',
+    license='',
+    author='lidian',
+    author_email='',
+    description=''
+)
--- test/test.py
+++ test/test.py
@@ -1,44 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tushare as ts
 
+d = ts.get_hist_data('600848')
 
-for line in open('../codes.txt'):
-    print(line)
+#print(len(d))
 
+#print(type(d))
 
-#print(open('../codes.txt'))
-exit()
+print(d.head(2))
 
-N = 5
-menMeans = (20, 35, 30, 35, 27)
-menStd =   (2, 3, 4, 1, 2)
+#print(d.head(1).describe())
 
-ind = np.arange(N)  # the x locations for the groups
-width = 0.35       # the width of the bars
+#print(d.loc[dates[0]])
+#print(d.index)
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(ind, menMeans, width, color='r', yerr=menStd)
-
-womenMeans = (25, 32, 34, 20, 25)
-womenStd =   (3, 5, 2, 3, 3)
-rects2 = ax.bar(ind+width, womenMeans, width, color='y', yerr=womenStd)
-
-# add some
-ax.set_ylabel('Scores')
-ax.set_title('Scores by group and gender')
-ax.set_xticks(ind+width)
-ax.set_xticklabels( ('G1', 'G2', 'G3', 'G4', 'G5') )
-
-ax.legend( (rects1[0], rects2[0]), ('Men', 'Women') )
-
-def autolabel(rects):
-    # attach some text labels
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-
-plt.show()
+#print(d.loc[:,['ma5', 'ma10']])
+#print(d.loc['2016-01-06':'2016-01-07'])
+print(d[['ma5', 'ma10']][d['ma5'] < 20])
+#print(d['ma5'])
