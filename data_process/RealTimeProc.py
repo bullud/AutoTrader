c97f6163a659a0b80e4cb7bf75d8e72231e3b27e
bullud <bullud@126.com>
2016-01-19 00:01:28 +0800

    Create MplMainWindow

--- data_process/RealTimeProc.py
+++ data_process/RealTimeProc.py
@@ -1,17 +1,26 @@
 from common.oneMinute import *
 from peewee import *
+import datetime
 
 class realtimeProc:
     def __init__(self, monitor_list, manager):
         self._manager = manager
+        self._m1s = dict()
+        self._bids = dict()
+        yesterday = datetime.date.today() - datetime.timedelta(1)
+        #datetime.datetime.strptime(str(yesterday), '%Y-%m-%d')
+        print(yesterday)
 
         for code in monitor_list:
-            result = oneMinute.select().where(oneMinute.code == code).order_by(oneMinute.date_time.desc()).limit(1)
+            result = oneMinute.select().where((oneMinute.code == code) & (oneMinute.date_time > yesterday))\
+                .order_by(oneMinute.date_time.desc())
+
             if len(result) == 0:
-                self.monitor[code] = ''
+                self._m1s[code] = None
             else:
                 #print(result[0].date_time)
-                self.monitor[code] = result[0].date_time.strftime('%Y-%m-%d %H:%M:%S')
+                self._m1s[code] = result
+
 
     def proc(self, bds):
 
