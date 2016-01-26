c4b17a0db257ee659ed1d637908aa32856e284b3
bullud <bullud@126.com>
2016-01-26 23:39:26 +0800

    add test pandas code

--- test/DailyDataProc.py
+++ test/DailyDataProc.py
@@ -11,7 +11,34 @@ import msvcrt
 
 
 def main():
+    con = sqlite3.connect('basics.sqlite')
 
+    dfb = ts.get_stock_basics()
+    #print(dfb.head())
+    #print(len(dfb))
+    dfb.to_sql('basics', con, if_exists = 'replace')
+
+    con.close()
+
+    #print(dfb.index)
+    for i in dfb.index:
+        dateS = dfb.ix[i]['timeToMarket'].astype(str)
+        if dateS == '0':
+            print('not time for code:' + i )
+            continue
+
+        date = datetime.datetime.strptime(dateS, "%Y%m%d").date()
+
+        dayCount = (datetime.datetime.today().date() - date).days + 1
+
+        #for oneDate in [date + datetime.timedelta(years = n) for n in range(yearCount)]:
+
+        #dfi = ts.get_h_data(i)
+
+    #con = sqlite3.connect('day.sqlite')
+    #df = ts.get_today_all()
+    #print(len(dfa))
+    return
 
     con = sqlite3.connect('day.sqlite')
     df = ts.get_today_all()
