8c58d35f90dd89371069f83f132cb8ef7dae196f
bullud <bullud@126.com>
2016-02-02 19:11:24 +0800

    add realtimeProc

--- test/testTickToM1.py
+++ test/testTickToM1.py
@@ -21,25 +21,34 @@ def _high_price(g):
 
 rootdir = "G:\\work\\sourcecode\\AutoTrader\\test\\ticks"
 
+m1rootdir = "G:\\work\\sourcecode\\AutoTrader\\test\\m1s"
+
 stocks = []
 i = 0
 for parent, dirnames, filenames in os.walk(rootdir):
     for filename in filenames:
-        print(os.path.join(parent,filename))
+        #print(os.path.join(parent,filename))
 
         (shotname, extension) = os.path.splitext(filename)
         parts = shotname.split('_')
         print(parts)
+        if parts[0] == '002536':
+            continue
 
         con = sqlite3.connect(os.path.join(parent,filename))
         sql = "SELECT * from ticks"
         ticks = pd.read_sql(sql, con)
+        con.close()
+
+        #t = ticks['time']
+        #print(type(t[0]))
+
 
         del ticks['index']
         del ticks['type']
         del ticks['change']
 
-        print(ticks.head())
+        #print(ticks.head())
 
         ticks.insert(3, 'low',   ticks['price'])
         ticks.insert(4, 'open',  ticks['price'])
@@ -47,31 +56,46 @@ for parent, dirnames, filenames in os.walk(rootdir):
         #print(ticks.head())
 
 
-
         TTime = pd.to_timedelta(ticks['time'])
         #stock['Time'] = TTime
         #TTime = pd.to_timedelta(TTime.dt.seconds - (TTime.dt.seconds % 60))
         f = lambda x: datetime.timedelta(seconds = (x.item() - x.item() %60000000000)/1000000000)
+        f2 = lambda x: str(datetime.timedelta(seconds = (x.item() - x.item() %60000000000)/1000000000))
 
-        ticks.insert(2,'timeIndex', TTime.map(f))
+        ticks.insert(2, 'timeIndex', TTime.map(f))
+        ticks.insert(3, 'timeIndex2', TTime.map(f2))
 
         #ticks.columns = ['date', 'time', 'price', '', 'HighPrice', 'Volume', 'Acount']
-        ticks.columns = ['date', 'time', 'timeIndex', 'high', 'low', 'open', 'close', 'volume', 'amount']
+        ticks.columns = ['date', 'time', 'timeIndex', 'timeIndex2', 'high', 'low', 'open', 'close', 'volume', 'amount']
 
-        print(ticks.head())
+        #print(ticks.head())
         #stocks.append(ticks)
 
-        grouped = ticks.groupby(['timeIndex'])
+        grouped = ticks.groupby(['date', 'timeIndex'])
         group = grouped.agg({'low':'min', \
                              'high': _high_price, \
                              'open':'first',\
                              'close':'last', \
                              'volume':'sum', \
-                             'amount':'sum'})
+                             'amount':'sum',
+                             'timeIndex2':'first'})
+
+        group.reset_index(inplace = True)
+
+        f2 = lambda x:x.astype(datetime)
+        #group['timeIndex'] = group['timeIndex'].astype(datetime)
+        group['timeIndex'] = group['timeIndex2']
+        del group['timeIndex2']
 
         print(group.head())
+        print(os.path.join(m1rootdir, parts[0] + '_m1.db'))
+
+
+        con2 = sqlite3.connect(os.path.join(m1rootdir, parts[0] + '_m1.db'))
+        group.to_sql('m1', con2, if_exists = 'replace', index = False)
+        con2.close()
         i += 1
-        if i == 1:
+        if i == 350:
             break
 
 exit()
